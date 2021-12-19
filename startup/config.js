274bc01affd64aefc13edf3d56cbb03b3ab1b0dc
Utkarsh Dhiman <47657465+utkarsh48@users.noreply.github.com>
2021-12-19 18:51:41 +0000

    mod: allow cors

--- startup/config.js
+++ startup/config.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("express-async-errors");
+const cors = require("cors");
+
 
 module.exports = function (app){
 	// app.use(apiKeyAuth);
 	// load env vars
-	
+	app.use(cors());
 	app.use(express.json());
 	app.use((err, req, res, next) => {
 		if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
